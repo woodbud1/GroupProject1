33064f835bd310828209c53ebc3a22da0cd55c6a
Dylan Kirk <dylankirk16@outlook.com>
2020-10-07 17:50:38 -0500

    questions

---
+++ model/questions.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class questions {
+    
+    private $id, $question, $answer;
+
+    public function __construct($question, $answer) {
+        $this->question = $question;
+        $this->answer = $answer;
+    }
+    
+    public function getID() {
+        return $this->id;
+    }
+    public function setID($value) {
+        $this->id = $value;
+    }
+
+    public function getQuestion() {
+        return $this->question;
+    }
+    public function setQuestion($value) {
+        $this->question = $value;
+    }
+
+    public function getAnswer() {
+        return $this->answer;
+    }
+    public function setAnswer($value) {
+        $this->answer = $value;
+    }
+}
