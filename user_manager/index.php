e3b5f92c7f6a465d3801722e14cb4d0e06ff86b1
airborne17th <43795410+airborne17th@users.noreply.github.com>
2020-10-12 19:18:01 -0500

    Drills Prototype
    Ran out of time, but I have functioning drills. Missing features!

--- user_manager/index.php
+++ user_manager/index.php
@@ -507,10 +507,77 @@ case "viewdelete":
         include('../about/index.php');
         break;
     case 'drill':
-        include('drill.php');
+        $drill_body = "Answer Questions below.";
+        $score = 0;
+        $count = 0;
+        $newFdigit = randdigit();
+        $newSdigit = randdigit();
+        $newOperator = randop();
+        include('../drill/index.php');
+    break;
+    case 'drill_reset':
+        $drill_body = "Drill Reset! Answer Questions below.";
+        $count = 0;
+        $score = 0;
+        include('../drill/index.php');
+    break;
+    case 'drill_answer':
+        $firstDigit = $_POST['lho'];
+        $secondDigit = $_POST['rho'];
+        $operator = $_POST['op'];
+        $userAnswer = $_POST['answer'];
+        $count = $_POST['count'];
+        $score = $_POST['score'];
+        $answer = evaluate($firstDigit, $secondDigit, $operator);
+        
+        if($answer == $userAnswer)
+        {
+            $count = $count + 1;
+            $score = $score + 1;
+            $drill_body = "Correct! $score out of $count";
+        }
+        else
+        {   
+            $count = $count + 1;
+            $drill_body = "Incorrect! $score out of $count";
+        }
+        include('../drill/index.php');
     break;
     default:
         $error = "Definitely, not suppose to be redirected here.";
         include('../errors/error.php');
         break;
+}
+
+function randdigit() {
+    return mt_rand(0,9);
+} 
+
+function randop(){
+    $ops = array('+', '-', '*');
+    // pick a random index between zero and highest index in array.
+    $randnum = mt_rand(0,sizeof($ops)-1);
+    return $ops[$randnum];  // Use the index to pick the operator
+}
+
+/* function selectop(){
+    $ops = array('+', '-', '*');
+    return $ops[$selected_key];  // Use the index to pick the operator
+} */
+
+function evaluate($d1, $d2, $op) {
+    switch($op) {
+        case '+' : // addition
+            $result = $d1 + $d2;
+            break;
+        case '-' : // subtraction
+            $result = $d1 - $d2;
+            break;
+        case '*' : // multiplication
+            $result = $d1 * $d2;
+            break;
+        default :  // Unidentified, return safe value
+            $result = 0;
+    }
+    return $result;
 }
