927dc359d8ab08bad49c483e8db4dfbe622ad435
airborne17th <43795410+airborne17th@users.noreply.github.com>
2020-10-14 18:57:04 -0500

    Index Fixes More

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $action = filter_input(INPUT_POST, 'action');
 if ($action === NULL) {
     $action = filter_input(INPUT_GET, 'action');
     if ($action === NULL) {
-        $action = 'registration';
+        $action = 'home_index';
     }
 }
 switch ($action) {
