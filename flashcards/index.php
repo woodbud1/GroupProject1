5308873c1a9b2486ebaf51454ad35553f1f3f629
airborne17th <43795410+airborne17th@users.noreply.github.com>
2020-10-15 14:28:20 -0500

    Flashcard Prototype

--- flashcards/index.php
+++ flashcards/index.php
@@ -1,10 +1,30 @@
 <?php
-
 $the_title = "MathWiz | Flashcards";
 $pathcor = "../";
 require_once '../view/header.php';
 ?>
+    <link rel="stylesheet" href="../flashcards/style.css" />
+  <body>
+    <div class="flashcard-box">
+    <h1>
+      Flash Cards
+    </h1>
 
-<div class="box">
-<h1 style="color:black;">Flashcards</h1>
-<?php require_once '../view/footer.php'; ?>
+    <div id="cards-container" class="cards">
+    </div>
+
+    <div class="navigation">
+      <button id="prev" class="nav-button">
+        [Prev]
+      </button>
+
+      <p id="current"></p>
+
+      <button id="next" class="nav-button">
+        [Next]
+      </button>
+    </div>
+</div>
+    <script src="../flashcards/flashcards.js"></script>
+  </body>
+</html>
